d57dcb32d730cf7738ecd7e58dca7fdd388c9758
rajch <rajch@Raj>
2024-02-29 08:37:53 +0530

    initial comment

---
+++ JavaBasics/src/JavaProgram/nested_if_else.java
@@ -0,0 +1,24 @@
+package JavaProgram;
+
+public class nested_if_else 
+{
+
+	public static void main(String[] args) 
+	{
+		int age=10;
+		if(age>18)
+		{
+			if(age>20)
+			{
+				
+			}
+			else if(age==18)
+			{
+				
+			}
+		}
+		else
+
+	}
+
+}
