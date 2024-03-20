b0d99f09dd21ff0b0c8ba87b6b5531d46dd40420
rajch <rajch@Raj>
2024-03-20 08:06:38 +0530

    03

--- JavaBasics/src/String/String_Arrays3.java
+++ JavaBasics/src/String/String_Arrays3.java
@@ -12,3 +12,4 @@ public class String_Arrays3
 }
 
 //Q. create array of size 4 but stores 5 values in it and let me know the exceptions that you face. 
+//Q. check the given two arrays are equal to each others.
